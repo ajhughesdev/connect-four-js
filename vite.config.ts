ef95aa0df82b6dc1f6d0dc2c9de1f260df1af8f9
ajhughesdev <andrew@ajhughes.dev>
2023-05-20 16:30:06 -0400

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/connect-four-js",
   plugins: [react(), svgr()],
   server: {
     watch: {
