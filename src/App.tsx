b6ae221253c11cef669d18ae7d4e9a53747f5caa
ajhughesdev <andrew@ajhughes.dev>
2023-05-23 05:18:30 -0400

    vercel

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import { Route, Routes } from 'react-router-dom'
 import MainMenu from './pages/MainMenu'
 import ConnectFour from './pages/ConnectFour'
 import Rules from './pages/Rules'
+import { inject } from '@vercel/analytics'
 
 const App = () => {
+  inject()
 
   return (
     <div className="App">
